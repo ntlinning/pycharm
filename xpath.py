074cc1b01cb197610b89f4cdf015c2700f6bc959
linning <614027356@qq.com>
2018-12-04 16:37:00 +0800

    4

--- xpath.py
+++ xpath.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 b = webdriver.Firefox()
 b.get('file:///Users/linning/Desktop/1.html')
-ele = b.find_element_by_xpath('//input[1]')
+ele = b.find_element_by_xpath('///input[1]')    #  相对路径
 ele.clear(),ele.send_keys('1111')
-ele2 = b.find_element_by_xpath('//input[2]')
+ele2 = b.find_element_by_xpath('/html/body/form/input[2]')    #绝对路径
 ele2.clear(),ele2.send_keys('2222')
 print(ele.get_attribute('name'))
 print(ele2.get_attribute('name'))
---
+++ xpath2.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+b = webdriver.Firefox()
+b.get('file:///Users/linning/Desktop/学习/jb51-1.net/%5BJava.Web.开发源码%5D.XML.XSLT.Servlet.JSP.深入剖析实例应用/JSPLesson/AppendixA/form.html')
+ele = b.find_element_by_xpath('/html/body/form/table/tbody/tr[1]/td[2]/input')
+ele.send_keys('linning')
