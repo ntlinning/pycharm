4e452b905a5c8aa7154d2f58f9d5570bfd1d0efd
linning <614027356@qq.com>
2018-12-04 16:14:19 +0800

    4

--- xpath.py
+++ xpath.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
-b = webdriver.Firefox
-b.get(r'/Users/linning/Desktop/1.html')
+b = webdriver.Firefox()
+b.get('file:///Users/linning/Desktop/1.html')
+ele = b.find_element_by_xpath('//input[1]')
+ele.clear(),ele.send_keys('1111')
+ele2 = b.find_element_by_xpath('//input[2]')
+ele2.clear(),ele2.send_keys('2222')
+print(ele.get_attribute('name'))
+print(ele2.get_attribute('name'))
+b.close()
+
+
+
+
